9b7fb587893d6884051df03a2daaa21353e96420
RincolF <2417447592@qq.com>
2023-12-21 00:33:21 +0800

    我的页面也改了搜索框；主页轮播图美化

---
+++ ChinaBank/我的页面/搜索框.js
@@ -0,0 +1,38 @@
+//后续功能再完善数组
+var functionArray = ["转账", "收付款", "账户管理", "查看流水", "安全中心"];
+
+function showDropdown() {
+  var dropdown = document.getElementById("dropdown");
+  dropdown.innerHTML = "";
+  for (var i = 0; i < functionArray.length; i++) {
+    var divOfSearch = document.createElement("div");
+    divOfSearch.innerHTML = functionArray[i];
+    divOfSearch.addEventListener("click", function () {
+      document.getElementById("searchBox").value = this.innerHTML;
+      document.getElementById("dropdown").style.display = "none";
+    });
+    dropdown.appendChild(divOfSearch);
+  }
+  document.getElementById("dropdown").style.display = "block";
+}
+function search() {
+  var inputOfSearch = document.getElementById("searchBox").value.toUpperCase();
+  var dropdown = document.getElementById("dropdown");
+  dropdown.innerHTML = "";
+  for (var i = 0; i < functionArray.length; i++) {
+    if (functionArray[i].toUpperCase().includes(inputOfSearch)) {
+      var divOfSearch = document.createElement("div");
+      divOfSearch.innerHTML = functionArray[i];
+      divOfSearch.addEventListener("click", function () {
+        document.getElementById("searchBox").value = this.innerHTML;
+        document.getElementById("dropdown").style.display = "none";
+      });
+      dropdown.appendChild(divOfSearch);
+    }
+  }
+}
+window.addEventListener("click", function (event) {
+  if (event.target.id !== "searchBox" && event.target.parentNode.id !== "dropdown") {
+    document.getElementById("dropdown").style.display = "none";
+  }
+});
