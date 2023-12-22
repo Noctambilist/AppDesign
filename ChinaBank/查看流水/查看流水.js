79c6e0d59c0936c76a2e5c05b8189f5807d55ef6
RincolF <2417447592@qq.com>
2023-12-22 22:56:08 +0800

    优化了查看流水页面；添加了查看流水的部分事件

---
+++ ChinaBank/查看流水/查看流水.js
@@ -0,0 +1,71 @@
+const confirmButton = document.querySelector('.box3-5 button');
+const chooseBoard = document.querySelector('.box3');
+const chooseButton = document.querySelector('.box1-3 img');
+
+const textYear = document.querySelector('#year');
+const textYear1 = document.querySelector('#year1');
+const textMonth = document.querySelector('#month');
+const textMonth1 = document.querySelector('#month1');
+const textDay = document.querySelector('#day');
+const textDay1 = document.querySelector('#day1');
+const moneyLeft = document.querySelector('.box3-4-2-1 input');
+const moneyRight = document.querySelector('.box3-4-2-3 input');
+
+
+let aliveDetector = setInterval(() => {
+  if (textDay.value != '' && textDay1.value != '' && moneyLeft.value != '' && moneyRight.value != '') {
+    //confirmButton.style.background = 'linear-gradient(129.12deg, #446dff, rgba(99, 125, 255, .75))';
+    confirmButton.style.opacity = '1';
+    confirmButton.disabled = false;
+  } else {
+    confirmButton.style.opacity = '0.5';
+    confirmButton.disabled = true;
+  }
+}, 50);
+
+confirmButton.addEventListener('click', () => {
+  // chooseBoard.classList.remove('slideUp');
+  // chooseBoard.classList.add('slideDown');
+  if (Number(textYear1.value) < Number(textYear.value)) {
+    alert('请检查年份区间是否有误');
+  } else if (Number(textYear1.value) === Number(textYear.value)) {
+    if (Number(textMonth1.value) < Number(textMonth.value)) {
+      alert('请检查月份区间是否有误');
+    } else if (Number(textMonth1.value) === Number(textMonth.value)) {
+      if (Number(textDay1.value) < Number(textDay.value)) {
+        alert('请检查日期区间是否有误');
+      } else {
+        //同年同月同日的情况
+        if (Number(moneyRight.value) < Number(moneyLeft.value)) {
+          alert('请检查金额区间是否有误');
+        } else {
+          //传数据还没写
+          chooseBoard.style.display = 'none';
+        }
+      }
+    } else {
+      //同年，后月份大的情况
+      if (Number(moneyRight.value) < Number(moneyLeft.value)) {
+        alert('请检查金额区间是否有误');
+      } else {
+        //传数据还没写
+        chooseBoard.style.display = 'none';
+      }
+    }
+  } else {
+    //后年大
+    if (Number(moneyRight.value) < Number(moneyLeft.value)) {
+      alert('请检查金额区间是否有误');
+    } else {
+      //传数据还没写
+      chooseBoard.style.display = 'none';
+    }
+  }
+
+})
+
+chooseButton.addEventListener('click', () => {
+  // chooseBoard.classList.remove('slideDown');
+  chooseBoard.classList.add('slideUp');
+  chooseBoard.style.display = 'flex';
+})
