a3f255b6921a53302f50f95957359ab187594dd1
RincolF <2417447592@qq.com>
2023-12-25 01:05:40 +0800

    小优化

--- ChinaBank/首页/轮播图.js
+++ ChinaBank/首页/轮播图.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.swiper', {
+var mySwiper = new Swiper('#swiper1', {
   //direction: 'vertical', // 垂直切换选项
   loop: true, // 循环模式选项
 
@@ -24,4 +24,32 @@ var mySwiper = new Swiper('.swiper', {
     disableOnInteraction: false,
   },
   effect: 'fade',
+})
+
+var mySwiper1 = new Swiper('#swiper2', {
+  direction: 'vertical', // 垂直切换选项
+  loop: true, // 循环模式选项
+  // effect: "flip",
+  // 如果需要分页器
+  // pagination: {
+  //   el: '.swiper-pagination1',
+  // },
+
+  // 如果需要前进后退按钮
+  // navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  // },
+
+  // 如果需要滚动条
+  // scrollbar: {
+  //     el: '.swiper-scrollbar',
+  // },
+
+  autoplay: {
+    delay: 1500,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
+  },
+  // effect: 'fade',
 })
