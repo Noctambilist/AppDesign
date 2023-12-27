1a0c4f651913582394e258d0e97cb548d5f2d676
RincolF <2417447592@qq.com>
2023-12-27 23:21:56 +0800

    完善了三处短信请求，登录页还需要做前端验证我没改，修改密码和换绑页面还差调接口

--- ChinaBank/我的页面/设置页面/手机换绑页面/安全验证页面.js
+++ ChinaBank/我的页面/设置页面/手机换绑页面/安全验证页面.js
@@ -1,9 +1,24 @@
 const HuoQuYanZhengMaButton = document.querySelector('.box2-3 button');
 const exitButton = document.querySelector('.box1-1 img');
 
+
+const ShuRuYanZhengMa = document.querySelector('.verCode');//弹窗大窗体
+const closeTanChuang = document.querySelector('.verCode1 img');//关闭弹窗
+const passwordInput = document.querySelector('.verCode2 input');//弹窗密码框
+const confirmPasswordButton = document.querySelector('.verCode3 button');//确认密码
+const hint = document.querySelector('.verCode p');//提示密码错误
+
+const phoneNumber = document.querySelector('.box2-2-1 p');//当前账户手机号，需要在页面加载的时候连后端渲染
+
+let varifyToBack;
+
 function slideMe() {
   document.body.style.marginRight = "0"
   document.body.style.opacity = "1";
+}
+
+function getRandomVarify() {
+  return (Math.floor(Math.random() * (999999 - 100000 + 1) + 100000));
 }
 
 exitButton.addEventListener('click', () => {
@@ -15,5 +30,47 @@ exitButton.addEventListener('click', () => {
 })
 
 HuoQuYanZhengMaButton.addEventListener('click', () => {
-
+  varifyToBack = +getRandomVarify();  //生成给后端的验证码
+  /*
+  *
+  *
+  * 把这个传给后端，发短信
+  * 
+  */
+  console.log(varifyToBack);
+  document.getElementById("overlay").classList.add("show");
+  ShuRuYanZhengMa.classList.add("show");
 })
+
+closeTanChuang.addEventListener('click', () => {
+  ShuRuYanZhengMa.classList.remove("show");
+  passwordInput.value = '';
+  document.getElementById("overlay").classList.remove("show");
+})
+
+confirmPasswordButton.addEventListener('click', () => {
+  if (Number(passwordInput.value) === varifyToBack) {//直接前端做验证
+    document.body.style.marginRight = "-15%";
+    document.body.style.opacity = "0";
+    setTimeout(function () {
+      location.href = './手机换绑页面.html';
+    }, 100);
+  } else {
+    hint.style.visibility = 'visible';
+  }
+})
+
+
+
+
+
+let aliveDetector = setInterval(() => {
+  if (passwordInput.value.length == 6) {
+    confirmPasswordButton.style.opacity = '1';
+  } else {
+    confirmPasswordButton.style.opacity = '0.5';
+  }
+
+  // console.log(selecttt.options[selecttt.selectedIndex].text);
+
+}, 50)
---
+++ ChinaBank/我的页面/设置页面/手机换绑页面/手机换绑页面.js
@@ -0,0 +1,76 @@
+const exitButton = document.querySelector('.box1-1 img');
+const getVarifyButton = document.querySelector('.box2-2 p');//获取验证码
+const phoneNumber = document.querySelector('.box2-2 .ta');//新手机号
+const confirmButton = document.querySelector('.box2-4 button');//确定按钮
+const varifyNumber = document.querySelector('.box2-3 textarea')//输入的验证码
+
+let varifyToBack;
+
+exitButton.addEventListener('click', () => {
+  document.body.style.marginRight = "-15%";
+  document.body.style.opacity = "0";
+  setTimeout(function () {
+    location.href = '../设置页面.html';
+  }, 100);
+})
+function slideMe() {
+  document.body.style.marginRight = "0"
+  document.body.style.opacity = "1";
+}
+function getRandomVarify() {
+  return (Math.floor(Math.random() * (999999 - 100000 + 1) + 100000));
+}
+
+getVarifyButton.addEventListener('click', () => {
+  varifyToBack = +getRandomVarify();  //生成给后端的验证码
+  console.log(varifyToBack);
+  /*
+  *
+  *
+  * 把这个传给后端，发短信
+  * 
+  */
+})
+
+confirmButton.addEventListener('click', () => {
+  if (Number(varifyNumber.value) === varifyToBack) {//直接前端做验证
+    /*
+    *
+    *
+    * 掉接口，把后端的手机号改了
+    * 
+    */
+
+
+    document.getElementById("success").classList.add("show");
+    document.getElementById("overlay").classList.add("show");
+    setTimeout(() => {
+      document.getElementById("success").classList.remove("show");
+      document.getElementById("overlay").classList.remove("show");
+    }, 1500)
+  }
+
+})
+
+
+
+let aliveDetector = setInterval(() => {
+  if (phoneNumber.value !== '') {
+    getVarifyButton.disabled = false;
+    getVarifyButton.style.color = 'blue';
+  } else {
+    getVarifyButton.disabled = true;
+    getVarifyButton.style.color = 'grey';
+  }
+
+  if (phoneNumber.value !== '' && varifyNumber.value !== '') {
+    getVarifyButton.disabled = false;
+    confirmButton.style.opacity = "1";
+  } else {
+    getVarifyButton.disabled = true;
+    confirmButton.style.opacity = "0.5";
+  }
+
+
+
+}, 50)
